e7f86f4d014372f61a301872139da26711e3179c
Sanjoy Sarker <sarkerd52@gmail.com>
2023-01-30 20:40:41 +0600

    Order functionality added

--- order/urls.py
+++ order/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('order/addtocart/<int:id>/',addToCart,name='addtocart'),
     path('order/deletefromcart/<int:id>/',deletefromcart,name='deletefromcart'),
     path('shopcart/',shopCart,name='shopcart'),
+    path('order/orderproduct/',orderProduct,name='orderproduct'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from product.models import *
 from django.contrib import messages
+from user.models import *
 from .models import *
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 @login_required(login_url='/login')
@@ -51,6 +54,8 @@ def shopCart(request):
     category = Category.objects.all()
     current_user = request.user
     shopcart = ShopCart.objects.filter(user_id=current_user.id)
+    total_items = sum([item.quantity for item in shopcart])
+    # total = sum([item.product.price * item.quantity for item in shopcart])
     total=0
     for rs in shopcart:
         total += rs.product.price * rs.quantity
@@ -58,6 +63,7 @@ def shopCart(request):
     context={'shopcart': shopcart,
              'category':category,
              'total': total,
+             'total_items':total_items
              }
     return render(request,'order/shopcart_products.html',context)
 
@@ -66,3 +72,70 @@ def deletefromcart(request,id):
     ShopCart.objects.filter(id=id).delete()
     messages.success(request, "Your item deleted form Shopcart.")
     return redirect("/shopcart")
+
+
+
+
+
+
+def orderProduct(request):
+    category = Category.objects.all()
+    current_user = request.user
+    shopcart = ShopCart.objects.filter(user_id=current_user.id)
+    total = sum([item.product.price * item.quantity for item in shopcart])
+    total_items = sum([item.quantity for item in shopcart])
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            data = Order()
+            data.first_name = form.cleaned_data['first_name']
+            data.last_name = form.cleaned_data['last_name']
+            data.address = form.cleaned_data['address']
+            data.city = form.cleaned_data['city']
+            data.country = form.cleaned_data['country']
+            data.phone = form.cleaned_data['phone']
+            data.user_id = current_user.id
+            data.total = total
+            data.ip = request.META.get('REMOTE_ADDR')
+            ordercode = get_random_string(5).upper()  # random code
+            data.code = ordercode
+            data.save()
+
+            for rs in shopcart:
+                detail = OrderProduct()
+                detail.order_id = data.id
+                detail.product_id = rs.product_id
+                detail.user_id = current_user.id
+                detail.quantity = rs.quantity
+
+                detail.price = rs.product.price
+                detail.amount = rs.amount
+                detail.save()
+
+                product = Product.objects.get(id=rs.product_id)
+                product.amount -= rs.quantity
+                product.save()
+
+            # Clear & Delete shopcart
+            ShopCart.objects.filter(user_id=current_user.id).delete()
+            request.session['cart_items'] = 0
+            messages.success(
+                request, "Your Order has been completed. Thank you ")
+            return render(request, 'order/order_completed.html', {'ordercode': ordercode,'category':category})
+        else:
+            messages.warning(request, form.errors)
+            return redirect("/order/orderproduct")
+
+    form = OrderForm()
+
+    profile = UserProfile.objects.get(user_id=current_user.id)
+    context = {'shopcart': shopcart,
+               'form': form,
+               'profile': profile,
+             'category':category,
+             'total': total,
+             'total_items':total_items
+               }
+    return render(request, 'order/order_form.html', context)
